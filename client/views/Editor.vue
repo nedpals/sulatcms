f3eced2d1c027ec33efcb0ce9cd03b9c5e9941de
Ned Palacios <npdoesmc@gmail.com>
2018-02-25 21:47:56 +0800

    pen wysiwyg now working

--- client/views/Editor.vue
+++ client/views/Editor.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="container">
-      {{ post($route.params.filename) }}
-      <textarea id="cms-editor"></textarea>
+  <div class="container" id="cms-editor">
+      <textarea v-html="post($route.params.filename).content"></textarea>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 const Pen = require('exports-loader?Pen!pen/src/pen')
-// const markdown = require('exports-loader?markdown!pen/src/markdown')
+import markdown from 'pen/src/markdown'
+Pen.markdown = markdown
 
 export default {
   mounted() {
-    this.initializePen(document.getElementById('cms-editor'))
+    const editor = this.initializePen(document.getElementById('cms-editor'))
+    console.log(Pen.markdown)
+    editor.focus()
+    editor.on("change", () => {
+      const val = this.editor.value()
+      this.$emit('input', val)
+    })
+
+    this.editor = editor
   },
   computed: {
     ...mapGetters({
