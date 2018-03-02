f8f799aed1aabc79c47b6b45744f8fc66e864b34
Ned Palacios <npdoesmc@gmail.com>
2018-03-02 19:15:42 +0800

    converting routes to mithril router

--- client/router/index.js
+++ client/router/index.js
@@ -1,26 +1,11 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import Home from '../views/Home'
 import Login from '../views/Login'
 import Editor from '../views/Editor'
 
-Vue.use(Router)
+const app = document.getElementById("app")
 
-export default new Router({
-  mode: 'hash',
-  routes: [
-    {
-      path: '/',
-      component: Home
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/edit/:filename',
-      component: Editor,
-      name: 'Edit'
-    }
-  ]
+export default m.route(app, '/', {
+  '/': Home,
+  '/login': Login,
+  '/edit/:filename': Editor
 })
