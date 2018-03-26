a8a4d5012c37dcc65dc39a08ca203e5f9f2f33ed
Ned Palacios <npdoesmc@gmail.com>
2018-03-26 10:28:15 +0800

    editor page now working

--- client/views/Editor.js
+++ client/views/Editor.js
@@ -1,9 +1,26 @@
 import SimpleMDE from 'simplemde'
 import "simplemde/dist/simplemde.min.css"
+import store from "../store"
 import format from "date-fns/format"
 
 export default {
+  post: {
+    title: '',
     date: format(Date.now(), 'YYYY-MM-DD'),
+    author: '',
+    tags: [],
+    filename: '',
+    content: ''
+  },
+  setContent(content) {
+    this.post.content = content
+  },
+  setValue(field, value) {
+    this.post[field] = value
+  },
+  oninit(vnode) {
+    vnode.state.post = store.state.posts.find(post => post.filename === vnode.attrs.filename) || vnode.state.post
+  },
   oncreate(vnode) {
     const md = this.initEditor(document.getElementById("cms-editor"), vnode.state.post.content)
     md.codemirror.focus()
@@ -26,6 +43,31 @@ export default {
       forceSync: true
     })
   },
+  view(vnode) {
+    return (
+      <div class="container grid-lg editor">
+        <form class="form-horizonal columns">
+        {Object.entries(vnode.state.post).map((fields) => {
+          return (
+            <div class={`form-group column ${(fields[0] === "title" ? "col-12 col-sm-12" : (fields[0] === "content" ? "col-12 col-sm-12" : `col-${12/(Object.keys(vnode.state.post).length-2)} col-sm-12`))}`}>
+              {fields[0] === "content" ? 
+              (
                 <textarea id="cms-editor"></textarea>
+              ) : (
+                  <div>
+                    {fields[0] === "title" ? null : (<label class="form-label">{fields[0].charAt(0).toUpperCase() + fields[0].slice(1)}</label>)}
+                    <input type="text"
+                      placeholder={`Insert ${fields[0]} here...`}
+                      class={`form-input ${fields[0]+"-field"} ${(fields[0] === "title" ? "h1 input-lg" : "")}`}
+                      value={vnode.state.post[fields[0]]}
+                      oninput={m.withAttr("value", (v) => { this.setValue(fields[0], v) })} />
+                  </div>
+              )}
+            </div>
+          )
+        })}
+        </form>
+      </div>
+    )
   }
 }
