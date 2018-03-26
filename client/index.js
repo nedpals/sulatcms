23aa43a9c53fba1959382bb542e3a10bd12deefa
Ned Palacios <npdoesmc@gmail.com>
2018-03-26 10:24:32 +0800

    wrapping layout for common pages

--- client/index.js
+++ client/index.js
@@ -1,15 +1,27 @@
-import * as m from 'mithril'
+const m = require('mithril')
 import "spectre.css/src/spectre.scss"
 import "spectre.css/src/spectre-icons.scss"
+import "../static/styles.scss"
 import Home from './views/Home.js'
 import Login from './views/Login.js'
 import Editor from './views/Editor.js'
+import App from './components/App'
 
 const app = document.getElementById("app")
 
+const v = (view, layout) => {
+    let rendered = {
+        render() {
+            return m((layout ? layout : App), m(view))
+        }
+    }
+
+    return rendered
+}
+
 m.route(app, '/', {
-    '/': Home,
-    '/login': Login,
+    '/': v(Home),
+    '/login': v(Login),
     '/edit/:filename': Editor,
     '/new': Editor
 })
