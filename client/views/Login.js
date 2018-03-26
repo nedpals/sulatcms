d81ec1d28a6bf9e13c1c200d963bc2a05d7d8238
Ned Palacios <npdoesmc@gmail.com>
2018-03-26 10:28:36 +0800

    converted to jsx

--- client/views/Login.js
+++ client/views/Login.js
@@ -1,18 +1,22 @@
 export default {
-  view: function() {
-    return m("div", {style: "margin-top: 6rem;"}, [
-      m(".columns", [
-        m("column.col-4.col-mx-auto" [
-          m("panel.bg-white" [
-            m(".panel-header" [
-              m("panel-title.text-center",
-                m("h1", "welcome"))
-            ]),
-            m(".panel-footer",
-              m("button", {class: "btn btn-block btn-lg btn-primary"}, "Login with Github"))
-          ])
-        ])
-      ])
-    ])
+  view() {
+    return (
+      <div style="margin-top:6rem;">
+        <div class="columns">
+          <div class="column col-4 col-mx-auto">
+            <div class="panel bg-white">
+              <div class="panel-header">
+                <div class="panel-title text-center">
+                  <h1>Welcome</h1>
+                </div>
+              </div>
+              <div class="panel-footer">
+                <button class="btn btn-block btn-lg btn-primary">Login with Github</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
