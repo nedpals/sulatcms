dd66f4214b9c27d599592bd72fb79a8af848a9b4
Ned Palacios <npdoesmc@gmail.com>
2018-03-30 13:31:46 +0800

    adding save button

--- client/components/OffCanvas.js
+++ client/components/OffCanvas.js
@@ -6,18 +6,25 @@ export default {
     },
     toggleMenu() {
         this.options.showMenu = !this.options.showMenu
-        console.log(this.options.showMenu)
     },
     view(vnode) {
         return (
             <div class={`off-canvas${vnode.attrs.customClass ? " " + vnode.attrs.customClass : "" }`}>
-                <div class="off-canvas-toggle">
-                    <button class="btn btn-primary btn-action" onclick={() => { this.toggleMenu() }}>
-                        <i class="icon icon-menu"></i>
-                    </button>
-                    <a href="/" class="btn btn-primary" oncreate={m.route.link}>
-                        Back to home
-                    </a>
+                <div class="off-canvas-toggle clearfix">
+                    <div class="float-left">
+                        <button class="btn btn-primary btn-action" onclick={() => { this.toggleMenu() }}>
+                            <i class="icon icon-menu"></i>
+                        </button>
+                        <a href="/" class="btn btn-primary" oncreate={m.route.link}>
+                            Back to home
+                        </a>
+                    </div>
+                    <div class="float-right">
+                        <a href="/" class="btn btn-primary" oncreate={m.route.link}>
+                            <i class="icon icon-check"></i>&nbsp;
+                             Save
+                        </a>
+                    </div>
                 </div>
 
                 <div class={`off-canvas-sidebar ${vnode.state.options.showMenu ? "active" : ""}`}>
