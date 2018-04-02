22dae48e7ef137a99d2834a1cc4e6607bae8ae92
Ned Palacios <npdoesmc@gmail.com>
2018-04-02 21:03:09 +0800

    props not working huhu

--- client/components/Search.js
+++ client/components/Search.js
@@ -1,8 +1,8 @@
 export default {
-    view() {
+    view(vnode) {
         return (
             <div class="has-icon-left">
-                <input type="text" class="form-input" placeholder="..." />
+                <input type="text" class="form-input" placeholder="..." value={vnode.attrs.value} oninput={m.withAttr("value", vnode.attrs.field)} />
                 <i class="form-icon icon icon-search"></i>
             </div>
         )
