b874a2cb47cbc595a06af568cc5597b7d6eb3627
Ned Palacios <npdoesmc@gmail.com>
2018-04-06 21:29:30 +0800

    updated globals state

--- client/store/index.js
+++ client/store/index.js
@@ -1,1 +1,11 @@
-const Globals = {}
+import Auth from "./auth"
+
+const Globals = { 
+    domain: '' || window.location.hostname,
+    git: {
+        user: Auth.user.username,
+        repo: "" 
+    }
+}
+
+export default Globals
