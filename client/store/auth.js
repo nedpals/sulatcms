43716aa3612b85c3be3581bfca6bfaae0f58ee83
Ned Palacios <npdoesmc@gmail.com>
2018-04-07 20:30:19 +0800

    netlify login implemented

--- client/store/auth.js
+++ client/store/auth.js
@@ -1,11 +1,26 @@
 import Authenticator from "netlify-auth-providers/src/netlify"
+import { defaults } from "../modules/git";
 
 const Auth = {
     state: {
         error: "",
         loggedIn: false,
-        data: {},
-        user: {}
+        data: {
+            provider: "",
+            token: "",
+            refresh_token: ""
+        },
+        user: {
+            handle: "",
+            name: {
+                first: "",
+                last: "",
+                full: ""
+            },
+            avatar: "",
+            email: "",
+            user_id: ""
+        }
     },
     settings: {
         provider: '',
@@ -15,10 +30,31 @@ const Auth = {
         return new Authenticator({site_id: this.settings.netlify_id})
     },
     authenticate(provider, callback) {
-        this.init().authenticate({provider: provider}, (err, data) => {
+        this.init().authenticate({provider: provider, scopes: defaults[provider].scopes }, (err, data) => {
             if (err) { this.state.error = err }
             this.state.data = data
             callback()
+            m.request({
+                method: "GET",
+                url: `${"https://gitlab.com/api/v4/"}user`,
+                headers: {
+                    'Authorization': 'Bearer ' + this.state.data.token
+                }
+            })
+            .then((currentUser) => {
+                this.state.loggedIn = true
+                this.state.user = {
+                    handle: currentUser.username,
+                    name: {
+                        first: currentUser.name.slice(" ")[0],
+                        last: currentUser.name.slice(" ")[1],
+                        full: currentUser.name
+                    },
+                    avatar: currentUser.avatar_url,
+                    email: currentUser.email,
+                    user_id: currentUser.id
+                }
+            })
         })
     }
 }
