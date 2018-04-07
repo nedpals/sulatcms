d913e0c002b92d1ab29e6e44d7dfdb394bb7d295
Ned Palacios <npdoesmc@gmail.com>
2018-04-07 20:27:50 +0800

    move post mock to its own file

---
+++ client/mocks/post-data.js
@@ -0,0 +1,21 @@
+const PostData = [
+    {
+        title: 'Post 1',
+        date: '2010-01-01',
+        author: 'Ned Palacios',
+        tags: ['hello', 'world'],
+        filename: 'post-1.md',
+        photo: 'post-1.md',
+        content: '# Hello world!'
+    },
+    {
+        title: 'Post 2',
+        date: '2010-01-01',
+        author: 'Ned Palacios',
+        tags: ['hello', 'world'],
+        filename: 'post-2.md',
+        content: '## hello rin, in h2'
+    }
+]
+
+export default PostData
--- client/store/post.js
+++ client/store/post.js
@@ -1,24 +1,8 @@
+import postData from "../mocks/post-data";
+
 const Post = {}
 Post.state = {
-    posts: [
-        {
-            title: 'Post 1',
-            date: '2010-01-01',
-            author: 'Ned Palacios',
-            tags: ['hello', 'world'],
-            filename: 'post-1.md',
-            photo: 'post-1.md',
-            content: '# Hello world!'
-        },
-        {
-            title: 'Post 2',
-            date: '2010-01-01',
-            author: 'Ned Palacios',
-            tags: ['hello', 'world'],
-            filename: 'post-2.md',
-            content: '## hello rin, in h2'
-        }
-    ]
+    posts: postData
 }
 
 Post.actions = {
