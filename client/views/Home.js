02f4f03e7fc842a6a97f4877a77e7afd7f1fb3f6
Ned Palacios <npdoesmc@gmail.com>
2018-04-09 20:17:16 +0800

    implementing data from github

--- client/views/Home.js
+++ client/views/Home.js
@@ -1,12 +1,20 @@
 import FileView from '../components/FileView'
 import Search from '../components/Search'
 import Post from '../store/post'
+import auth from '../store/auth'
+import App from '../components/App'
 
 export default {
   loading: false,
   searchText: "",
   setSearch(s) { 
     this.searchText = s
+  },
+  oninit() {
+    Post.actions.refreshList()
+  },
+  oncreate() {
+    m.redraw()
   },
   view(vnode) {
     return (
@@ -18,11 +26,11 @@ export default {
                 <a href="/new" oncreate={m.route.link} class="btn btn-primary float-left"><i class="icon icon-lg icon-plus"></i> Add new post</a>
                 <div class="float-right">
                   <div class="has-icon-left">
-                    <input type="text" 
-                           class="form-input" 
-                           placeholder="..." 
-                           value={vnode.state.searchText} 
-                           oninput={m.withAttr("value", (s) => {this.setSearch(s)})} 
+                    <input type="text"
+                      class="form-input"
+                      placeholder="..."
+                      value={vnode.state.searchText}
+                      oninput={m.withAttr("value", (s) => { this.setSearch(s) })}
                     />
                     <i class="form-icon icon icon-search"></i>
                   </div>
@@ -32,31 +40,21 @@ export default {
               <div>
                 <div class="columns">
                   <div class="column py-1 my-2 col-12">
-                    {vnode.state.loading ? (<div class="loading loading-lg text-center"></div>)
-                      : (Post.state.posts.length >= 1 ?
+                    {Post.state.posts.error ? Post.state.posts.error :
+                      Post.state.posts === [] ? (<div class="loading loading-lg text-center"></div>) :
                         Post.state.posts.map((post) => {
                           return [
                             <div key={post.filename}>
                               <h2 style="margin-bottom:0;">
                                 <a href={"/edit/" + post.filename} oncreate={m.route.link}>{post.title}</a>
                               </h2>
-                              <p class="text-uppercase text-gray">By {post.author} Filed under: {post.tags.join(', ')}</p>
+                              <p class="text-uppercase text-gray">By {post.author} Filed under: {post.tags}</p>
                             </div>
                           ]
-                        }) : (
-                          <div class="empty">
-                            <div class="empty-icon">
-                              <i class="icon icon-4x icon-mail"></i>
-                            </div>
-                            <p class="empty-title h5">You haven't write a single post!</p>
-                          </div>
-                        ))
+                        })
                     }
                   </div>
                 </div>
-              </div>
-              <div class="columns">
-
               </div>
             </div>
           </div>
