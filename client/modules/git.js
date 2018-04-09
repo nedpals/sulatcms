6ddfb490fa960faaeb6a325cef7fece22384fdb9
Ned Palacios <npdoesmc@gmail.com>
2018-04-09 20:15:28 +0800

    gitlab api basic endpoints now implemented

--- client/modules/git.js
+++ client/modules/git.js
@@ -1,56 +1,77 @@
-import Auth from "../store/auth"
 import Global from "../store"
 import Post from "../store/post"
 
-const defaults = {
-    github: {
-        base_url: "https://api.github.com/",
-        headers: {
-            'Accept': 'application/vnd.github.v3+json',
-            'Authorization': 'Bearer ' + Auth.state.data.token
-        }
-    },
-    gitlab: {
-        base_url: "https://gitlab.com/api/v4/",
-        scopes: "api,read_user,sudo,read_registry",
-        headers: {
-            'Authorization': 'token ' + Auth.state.data.token
-        }
-    },
-    bitbucket: {
-        base_url: "https://api.bitbucket.org/2.0/",
-        headers: {
-            'Authorization': 'Bearer ' + Auth.state.data.token
-        }
-    }
-}
-
-const endpoints = {
-    gitlab: {
-        fetch(type, options) {
-            return {
-                type: type,
-                path: "/projects/:repo/repository/tree",
-                data: { repo: `${Global.git.user}/${Global.git.repo}` }
+const gitApi = {
+    defaults: {
+        github: {
+            base_url: "https://api.github.com",
+            headers(token) {
+                return { 
+                    'Accept': 'application/vnd.github.v3+json',
+                    'Authorization': token ? ('Bearer ' + token) : undefined 
+                }
             }
-        }
-    },
-    github: {
-        fetch(type, options) {
-            return {
-                type: type,
-                path: "",
-                data: {}
+        }, 
+        gitlab: {
+            base_url: "https://gitlab.com/api/v4",
+            scopes: "api+read_user",
+            headers(token) {
+                return { 'Authorization': token ? ('Bearer ' + token) : undefined }
             }
         },
-        push() {
-            path: "/"
+        bitbucket: {
+            base_url: "https://api.bitbucket.org/2.0",
+            headers(token) {
+                return { 'Authorization': token ? ('Bearer ' + token) : undefined }
+            }
+        }
+    },
+    endpoints: {
+        gitlab: {
+            fetch(type, options) {
+                return {
+                    type: type,
+                    path: "/projects/:repo/repository/tree",
+                    data: { 
+                        repo: encodeURIComponent(Global.repo),
+                        path: type === "path" ? options.path : undefined
+                    }
+                }
+            },
+            fetchFileRaw(blob_id) {
+                return {
+                    path: "/projects/:repo/repository/blobs/:blob_id/raw",
+                    data: {
+                        repo: encodeURIComponent(Global.repo),
+                        blob_id: blob_id
+                    },
+                    deserialize: (value) => { return value }
+                }
+            }
+        },
+        github: {
+            fetch(type, options) {
+                return {
+                    type: type,
+                    path: "",
+                    data: {}
+                }
+            },
+            push() {
+                path: "/"
+            }
         }
     }
 }
 
-function git(provider, endpoint) {
-   return "work in progress" 
+function gitDo (provider, token, endpoint) {
+    return m.request({
+        method: endpoint.method ? endpoint.method : "GET",
+        url: gitApi.defaults[provider].base_url + endpoint.path,
+        headers: gitApi.defaults[provider].headers(token),
+        data: endpoint.data,
+        deserialize: endpoint.deserialize ? endpoint.deserialize : undefined
+    })
 }
 
-export { defaults, endpoints, git }
+export { gitApi, gitDo }
