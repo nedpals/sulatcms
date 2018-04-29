54cc2534d1b326ffd8668648370d069cffb46683
Ned Palacios <npdoesmc@gmail.com>
2018-04-29 21:34:45 +0800

    testing plugin system

--- client/index.js
+++ client/index.js
@@ -2,25 +2,29 @@ import "../styles/styles.scss"
 import Routes from "./routes"
 import Auth from "./store/auth"
 import Global from "./store"
-
 import pluggable from "pluggable.js/src/pluggable.js"
+const app = {}
 
-const app = () => { this }
-app.initialize = (mount, options) => {
+app.printMsg = msg => {
+  console.log(msg)
+}
+
+app.initialize = (options, mount = document.getElementById('app')) => {
   document.title = "SulatCMS"
-  initializeRoutes(mount || arguments[0] || document.getElementById('app'))
-  options = arguments[1]
-
+  // Routes(mount || arguments[0] || document.getElementById('app'))
+  Routes(null)
   Auth.settings = {
     provider: options.auth.provider,
     netlify_id: options.auth.netlify_id
   }
   Global.domain = options.domain
   Global.repo = options.repo
+  app.pluginSocket.initializePlugins()
 }
 
 app.registerPlugin = (name, plugin) => {
   app.pluginSocket.registerPlugin(name, plugin)
+  // return "TODO"
 }
 
 pluggable.enable(app)
@@ -31,3 +35,13 @@ const _public = {
 }
 
 module.exports = _public
+
+if (process.env.NODE_ENV === "development") {
+  app.initialize({
+    auth: {
+      netlify_id: "7ed5abbf-556c-4bae-982c-225b15c3b997",
+      provider: "gitlab"
+    },
+    repo: "petreanvoice/db"
+  }, document.getElementById("sulat"))
+}
