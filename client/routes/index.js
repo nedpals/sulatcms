da21b5d843273320946ba5c375a632d606c1399a
Ned Palacios <npdoesmc@gmail.com>
2018-05-01 17:52:45 +0800

    fixed route

--- client/routes/index.js
+++ client/routes/index.js
@@ -1,30 +1,51 @@
-import Home from '../views/Home.js'
-import Login from '../views/Login.js'
-import Editor from '../views/Editor.js'
+import * as View from '../views'
 import App from '../components/App'
 import Auth from '../store/auth'
 
 export default function initRoutes(mount) {
-    const v = (view, layout) => {
-        let rendered = {
-            render() {
-                return m((layout ? layout : App), m(view))
-            }
-        }
+  const v = (view, layout) => {
+    return m((layout ? layout : App), view)
+  }
 
-        return rendered
+  const auth = localStorage.getItem("auth_token") || Auth.loggedIn
+
+  return m.route(mount, '/', {
+    '/': {
+      onmatch(args, requestedPath) {
+        if (!auth) return m.route.set('/login')
+        else return View.Home
+      },
+      render(vnode) {
+        return v(vnode)
+      }
+    },
+    '/login': {
+      onmatch(args, requestedPath) {
+        if (auth) return m.route.set('/')
+        else return View.Login
+      },
+      render(vnode) {
+        return vnode
+      }
+    },
+    '/edit/:key': {
+      onmatch(args, requestedPath) {
+        if (!auth) return m.route.set('/login')
+        else return View.Editor
+      },
+      render(vnode) {
+        return vnode
+      }
+    },
+    '/new': {
+      onmatch(args, requestedPath) {
+        if (!auth) return m.route.set('/login')
+        else return View.Editor
+      },
+      render(vnode) {
+        return vnode
+      }
     }
-
-    return m.route(mount, '/', {
-        '/': Login,
-        '/home': {
-            onmatch() {
-                if (!Auth.state.loggedIn) m.route.set("/")
-                else return Home
-            }
-        },
-        '/edit/:key': Editor,
-        '/new': Editor
-    })
+  })
 }
 
