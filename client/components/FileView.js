1b4f48bba3f2c4480be17364d02eb1e8e26d89b6
Ned Palacios <npdoesmc@gmail.com>
2018-05-04 15:07:24 +0800

    adding (non-functional) search bar to fileview

--- client/components/FileView.js
+++ client/components/FileView.js
@@ -1,10 +1,12 @@
 import store from '../store/post'
+import Search from "./Search"
 
 export default {
   view(vnode) {
     return (
       <div class="column p-2 bg-gray" style="height:100vh;">
         <a class="btn btn-primary btn-lg btn-block" href="/new" oncreate={m.route.link}>New</a>
+        <div class="mt-2">{m(Search)}</div>
         <ul class="nav">
           {
           store.state.posts.map((post) => {
