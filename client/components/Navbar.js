3fd5e3094830b00afc11a78b05fcef1db007b5cb
Ned Palacios <npdoesmc@gmail.com>
2018-05-04 15:13:42 +0800

    updated navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -7,50 +7,53 @@ export default {
     toggleMenu() {
         this.menuActive = !this.menuActive
     },
+    endSession() {
+      Auth.logout()
+    },
     view(vnode) {
-        return (
-            <header class="navbar sulat-navbar px-2 py-1">
-                <section class="navbar-section">
-                    <span class="p-2">{Global.domain}</span>
-                </section>
-                <section class="navbar-center">
-                    <h1 class="m-0" style="text-align:center;">
-                        <img src={logo} class="img-responsive" style="width:18%;max-width:18%;margin:auto;" />
-                    </h1>
-                </section>
-                <section class="navbar-section">
-                    <div class={`dropdown dropdown-right ${vnode.state.menuActive ? "active" : ""}`}>
-                        <button onclick={() => {this.toggleMenu()}} class="btn btn-link p-0" tabindex="0">
-                            <span class="mr-2">{Auth.state.user.handle}</span>
-                            <i class="icon icon-caret"></i>
-                        </button>
+      return (
+        <header class="navbar sulat-navbar px-2 py-1">
+            <section class="navbar-section">
+                <span class="p-2">{Global.domain}</span>
+            </section>
+            <section class="navbar-center">
+                <h1 class="m-0" style="text-align:center;">
+                    <img src={logo} class="img-responsive" style="width:18%;max-width:18%;margin:auto;" />
+                </h1>
+            </section>
+            <section class="navbar-section">
+                <div class={`dropdown dropdown-right ${vnode.state.menuActive ? "active" : ""}`}>
+                    <button onclick={() => {this.toggleMenu()}} class="btn btn-link p-0" tabindex="0">
+                        <span class="mr-2">{Auth.state.user.handle}</span>
+                        <i class="icon icon-caret"></i>
+                    </button>
 
-                        <ul class="menu">
-                            <li class="menu-item">
-                                <div class="tile tile-centered">
-                                    <div class="tile-icon">
-                                        <img src={`${Auth.state.user.avatar}`} class="avatar" alt={`${Auth.state.user.name.full} (${Auth.state.user.handle})`} />
-                                    </div>
-                                    <div class="tile-content">
-                                        {Auth.state.user.name.full}
-                                    </div>
+                    <ul class="menu">
+                        <li class="menu-item">
+                            <div class="tile tile-centered">
+                                <div class="tile-icon">
+                                    <img src={`${Auth.state.user.avatar}`} class="avatar" alt={`${Auth.state.user.name.full} (${Auth.state.user.handle})`} />
                                 </div>
-                            </li>
-                            <li class="divider"></li>
-                            {/* <li class="menu-item">
-                                <a href="#menus">
-                                Settings
-                                </a>
-                            </li> */}
-                            <li class="menu-item">
-                                <a href="#menus">
-                                Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </section>
-            </header>
-        )
+                                <div class="tile-content">
+                                    {Auth.state.user.name.full}
+                                </div>
+                            </div>
+                        </li>
+                        <li class="divider"></li>
+                        {/* <li class="menu-item">
+                            <a href="#menus">
+                            Settings
+                            </a>
+                        </li> */}
+                        <li class="menu-item">
+                            <button onclick={() => {this.endSession()}}>
+                            Logout
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </header>
+      )
     }
 }
