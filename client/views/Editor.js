5dfda0c9a73f0c004f26f5bd9f8105e375976bb1
Ned Palacios <npdoesmc@gmail.com>
2018-12-05 22:18:03 +0800

    editor now functioning

--- client/views/Editor.js
+++ client/views/Editor.js
@@ -5,49 +5,80 @@ import { fire } from "../modules/pluginSystem"
 import { stringify } from "mdify"
 import format from "date-fns/format"
 import parseTime from "date-fns/parse"
+import Globals from "../store"
 
 export default {
   post: {
-    attributes: {
+    metadata: {
       title: '',
-      date: format(Date.now(), 'YYYY-MM-DD'),
-      author: '',
+      date: format(parseTime(new Date())),
+      author: [],
       tags: [],
     },
     file_path: '',
     filename: '',
     contents: ''
   },
-  setContent(field, content) {
-    field = content
+  setContent(value) {
+    this.post.contents = value
+  },
+  getValue(field) {
+    if (typeof(field) === Array) {
+      return field.join(", ")
+    }
+
+    return field
   },
   setValue(field, value) {
-    this.post.attributes[field] = value
+    this.post.metadata[field] = value
+
+    if ((field !== 'title' && value.includes(",")) || Array.isArray(this.post.metadata[field])) {
+      this.post.metadata[field] = [...(value.split(","))]
+    }
+
+    console.log(this.post.metadata[field])
+
+    if (field === 'title' && m.route.get() === '/new') {
+      let newFilename = `${format(this.post.metadata.date, 'YYYY MM DD')}  ${value.substring(0, 30)}.md`.replace(/\s+/g, '-').toLowerCase()
+      this.post.filename = newFilename
+      this.post.file_path = `${Globals.keys.posts_path}${newFilename}`
+    }
   },
   savePost() {
-    if (this.post.updated === '') {
+    if (!this.post.metadata.hasOwnProperty('updated')) {
       this.post.updated = format(parseTime(new Date()))
     }
 
-    let output = stringify(this.post.attributes, this.post.content)
+    let output = stringify(this.post.metadata, this.post.contents)
     store.actions.savePost(this.post.file_path, this.post.filename, output)
   },
   oninit(vnode) {
+    window.scrollTo(0,0)
+    
     vnode.state.post = store.state.posts.find(post => post.filename === vnode.attrs.key) || vnode.state.post
-  },
-  oncreate() {
-    m.redraw()
-
     fire('editor.initialize')
   },
-  onupdate() {
-    console.log(this.post)
+  oncreate() {
+    if (m.route.get() === '/new') {
+      if (!this.post.metadata.hasOwnProperty('updated')) {
+        this.post.updated = format(parseTime(new Date()))  
+      }
+      let newTitle = this.post.metadata.title.substring(0, 30)
+  
+      this.post.filename = this.post.filename || `${format(this.post.metadata.date, 'YYYY MM DD')}  ${newTitle}.md`.replace(/\s+/g, '-').toLowerCase()
+      this.post.file_path = this.post.file_path || `${Globals.keys.posts_path}${this.post.filename}`
+    }
+  },
+  onbeforeupdate(vnode, old) {
+    if (vnode.attrs.key !== old.attrs.key) {
+      window.scrollTo(0,0)
+    }
   },
   view(vnode) {
-    return m(OffCanvas, {customClass: "editor-view", currentId: this.post.filename, actions: { save: () => { this.savePost() }, delete: () => { store.actions.deletePost() } }}, (
+    return m(OffCanvas, {customClass: "editor-view", currentId: this.post.filename, actions: { save: () => { this.savePost() }, delete: () => { store.actions.deletePost(Globals.keys.file) } }}, (
       <div class="container grid-md editor">
         <form class="form-horizonal columns">
-          {Object.entries(this.post.attributes).map((fields) => {
+          {Object.entries(this.post.metadata).map((fields) => {
             return (
               <div
                 class={`form-group column ${(fields[0] === "title" ? "col-12 col-sm-12" :
@@ -57,12 +88,12 @@ export default {
                      col-sm-12`))}`
                 }
               >
-                {m(Component.Textbox, { field: fields[0], fieldValue: this.post.attributes[fields[0]], setValue: this.setValue.bind(this) })}
+                {m(Component.Textbox, { field: fields[0], fieldValue: this.getValue(this.post.metadata[fields[0]]), setValue: this.setValue.bind(this) })}
               </div>
             )
           })}
           <div class="col-12 col-sm-12">
-            {m(Component.RichText, { content: this.post.contents, setContent: (value) => this.setContent(this.post.contents, value) })}
+            {m(Component.RichText, { value: vnode.state.post.contents, onchange: (v) => this.setContent(v) })}
           </div>
         </form>
       </div>
