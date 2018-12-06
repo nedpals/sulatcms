a19b88e119232461d68f08995b7d6757c8d45c62
Ned Palacios <npdoesmc@gmail.com>
2018-12-06 22:06:25 +0800

    github support: fetching list now working

--- client/plugins/git/hooks/github.js
+++ client/plugins/git/hooks/github.js
@@ -1,5 +1,53 @@
+import { gitDo, gitApi } from "../git";
+import fm, { fmTest } from "../../../utilities/fm"
+
 export default {
-    hooks: {
-        // TODO: Github platform hooks
+    beforePublish(filepath, filename, payload, search) {
+        let _commit_message = ''
+
+        gitDo(
+            gitApi.endpoints.github[search(filename) ? 'updateFile' : 'createFile'],
+            filepath,
+            {
+                branch: Globals.branch,
+                content: payload,
+                message: _commit_message
+            }
+        )
+    },
+    loadList(cb, path) {
+        gitDo(gitApi.endpoints.github.fetch("master"))
+        .then(({tree}) => {
+            return tree.filter(file => {
+                return file.path.includes(path)
+            })
+        })
+        .then(files => {
+            return Promise.all(
+                files.map(file => {
+                    return gitDo(gitApi.endpoints.github.fetchFileRaw(file.url))
+                })
+            )
+                .then(fileContents => {
+                    return files.map((file, i) => {
+                        let obj = {
+                        metadata:
+                            fmTest(fileContents[i]) && fm(fileContents[i]).attributes,
+                        file_path: file.path,
+                        filename: file.path.replace(path, ''),
+                        contents: fmTest(fileContents[i])
+                            ? fm(fileContents[i]).body
+                            : fileContents[i]
+                        }
+
+                        return obj
+                    })
+                })
+                .catch(err => console.error(err))
+        })
+        .then(posts => cb(posts, null))
+        .catch(err => {
+            cb(null, err)
+        })
     }
 }
