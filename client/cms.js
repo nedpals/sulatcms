2645e300c59dd8e0ad64e0c1371401356bca00d4
Ned Palacios <npdoesmc@gmail.com>
2018-12-09 17:16:55 +0800

    hardcode init git plugin

--- client/cms.js
+++ client/cms.js
@@ -2,6 +2,7 @@ import Routes from "./routes"
 import Auth from "./store/auth"
 import Global from "./store"
 import { initializePlugins, fire } from "./modules/pluginSystem"
+import { gitPlugin } from "./plugins/git/plugin"
 
 function initialize(options, mount = document.body) {
   document.title = "SulatCMS"
@@ -9,21 +10,23 @@ function initialize(options, mount = document.body) {
   
   Object.keys(options).map(option => {
     if (typeof (options[option]) === 'object') {
-    if (option.includes('auth')) {
-      Object.keys(options[option]).map(authOpt => {
-        Auth.settings[authOpt] = options[option][authOpt]
-      })
+      if (option.includes('auth')) {
+        Object.keys(options[option]).map(authOpt => {
+          Auth.settings[authOpt] = options[option][authOpt]
+        })
       } else {
-      Object.keys(options[option]).map(nestedOpt => {
-        Global[option][nestedOpt] = options[option][nestedOpt] || (Global[option][nestedOpt] || "")
-      })
-    }
+        Object.keys(options[option]).map(nestedOpt => {
+          Global[option][nestedOpt] = options[option][nestedOpt] || (Global[option][nestedOpt] || "")
+        })
+      }
     }
 
     Global[option] = options[option] || (Global[option] || "")
   })
   
-  initializePlugins()
+  initializePlugins([
+    gitPlugin()
+  ])
 
   const settings = Auth.settings
   fire('auth.initialize', [settings])
