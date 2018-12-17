208324cf0d2c4a4ce6fa944085d56162eba4e4e6
Ned Palacios <npdoesmc@gmail.com>
2018-12-17 17:41:48 +0800

    simplified entry of functions

--- client/index.js
+++ client/index.js
@@ -2,9 +2,7 @@ import "../styles/styles.scss"
 import { initialize } from "./cms"
 import { registerPlugin } from "./modules/pluginSystem"
 
-const _public = {
+module.exports = {
   initialize,
   registerPlugin
 }
-
-module.exports = _public
